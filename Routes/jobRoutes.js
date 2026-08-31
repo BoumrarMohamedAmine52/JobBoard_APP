70c458cc7bac0e9460dd8b21b85020a30a16e104
BoumrarMohamedAmine <boumrar.mohamedamine5252@gmail.com>
2026-08-31 19:08:00 +0200

    initialize delete and update routes

--- Routes/jobRoutes.js
+++ Routes/jobRoutes.js
@@ -6,6 +6,8 @@ const jobControllers = require("../Controllers/jobController");
 
 Router.route("/").get(jobControllers.getAllJobs).post(jobControllers.addJob);
 
-Router.route("/:id").get(jobControllers.getJob);
+Router.route("/:id")
+  .get(jobControllers.getJob)
+  .delete(jobControllers.deleteJob);
 
 module.exports = Router;
