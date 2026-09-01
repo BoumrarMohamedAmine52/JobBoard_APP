642b2f9fcb89c9a0e85322c59f399f8631259f24
BoumrarMohamedAmine <boumrar.mohamedamine5252@gmail.com>
2026-09-01 16:16:13 +0200

    create the givePermissionTo function

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -98,3 +98,14 @@ exports.protect = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.givePermissionTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new Error("You do not have the permission to perform this action."),
+      );
+    }
+    next();
+  };
+};
