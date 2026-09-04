3161034447f4a2d43cc68e527722345cc3aca021
BoumrarMohamedAmine <boumrar.mohamedamine5252@gmail.com>
2026-09-04 17:49:03 +0200

    create custome error class AppError

---
+++ Utils/AppError.js
@@ -0,0 +1,12 @@
+class AppError extends Error(){
+    Constructor (message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error',
+
+        Error.captureStackTrace(this, this.constuctor);
+    }
+}
+
+module.exports = AppError;
